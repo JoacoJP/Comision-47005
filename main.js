3393e5fe81c99a9a1dfeadff696f22036286fcfb
JoacoJP <133238083+JoacoJP@users.noreply.github.com>
2023-09-11 10:55:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,60 @@
-function calculateTotal() {
-    const productPrices = [8500, 30000, 15000];
-    const totalProducts = productPrices.length;
-    let totalCost = 0;
+const productsArray1 = [
+  { name: "Producto 1", price: 10000 },
+  { name: "Producto 2", price: 30000 },
+  { name: "Producto 3", price: 15000 }
+];
 
-    // Recorremos los productos y sumamos el costo total
-    for (let i = 0; i < totalProducts; i++) {
-    const quantity = parseInt(document.getElementById("product" + (i + 1)).value);
+const productsArray2 = [
+  { name: "Producto A", price: 8500 },
+  { name: "Producto B", price: 25000 },
+  { name: "Producto C", price: 10000 }
+];
 
-      // Verificamos si la cantidad es válida (mayor o igual a cero)
+function calculateTotal(products) {
+  const totalCost = products.reduce((total, product, index) => {
+    const quantity = parseInt(document.getElementById("product" + (index + 1)).value);
+    
     if (!isNaN(quantity) && quantity >= 0) {
-        totalCost += productPrices[i] * quantity;
+      return total + product.price * quantity;
+    } else {
+      return total;
     }
-    }
+  }, 0);
 
-    // Mostramos el resultado al usuario
-    document.getElementById("totalCost").innerText = "El costo total es: $" + totalCost;
+  document.getElementById("totalCost").innerText = "El costo total es: $" + totalCost;
 
+  localStorage.setItem('totalCost', totalCost);
 }
 
+async function fetchDataFromAPI() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1'); 
+    if (!response.ok) {
+      throw new Error('Error al cargar los datos');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error al obtener datos:', error);
+    return [];
+  }
+}
+
+window.addEventListener('load', async function() {
+  const storedTotalCost = localStorage.getItem('totalCost');
+  if (storedTotalCost) {
+    document.getElementById("totalCost").innerText = "El costo total es: $" + storedTotalCost;
+  }
+
+  // Cargar datos de la API de manera asíncrona
+  const apiData = await fetchDataFromAPI();
+  // Puedes utilizar 'apiData' en tu lógica aquí
+});
+
+document.getElementById("calculateWithIVA").addEventListener("click", function() {
+  calculateTotal(productsArray1, true);
+});
+
+document.getElementById("calculateWithoutIVA").addEventListener("click", function() {
+  calculateTotal(productsArray2, false);
+});
